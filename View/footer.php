972fa03c64dad3862ab1b96274e5c3fb3279eca9
philipsvo <41573444+philipsvo@users.noreply.github.com>
2018-09-16 19:16:36 +0700

    headerAndFooterInIndex
    include header va footer trong index

---
+++ View/footer.php
@@ -0,0 +1,163 @@
+<!-- Footer -->
+<footer class="footer footer--dark footer--bg-img"
+        style="background-image: url('img/content/footer/footer_bg_img.jpg');">
+    <div class="container">
+        <div class="footer__widgets">
+            <div class="footer__widgets-top">
+                <div class="row justify-content-between align-items-center">
+                    <div class="col-md-4">
+                        <a href="index.html">
+                            <img src="img/logo_default_white.png"
+                                 srcset="img/logo_default_white.png 1x, img/logo_default_white@2x.png 2x"
+                                 class="logo__img" alt="">
+                        </a>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="socials socials--large socials--nobase justify-content-md-end">
+                            <a href="#" class="social social-facebook" aria-label="facebook"><i
+                                    class="ui-facebook"></i></a>
+                            <a href="#" class="social social-twitter" aria-label="twitter"><i
+                                    class="ui-twitter"></i></a>
+                            <a href="#" class="social social-google-plus" aria-label="google+"><i
+                                    class="ui-google"></i></a>
+                            <a href="#" class="social social-youtube" aria-label="youtube"><i
+                                    class="ui-youtube"></i></a>
+                            <a href="#" class="social social-instagram" aria-label="instagram"><i
+                                    class="ui-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-2 col-md-6">
+                    <aside class="widget widget_nav_menu">
+                        <h4 class="widget-title">Useful Links</h4>
+                        <ul>
+                            <li><a href="about.html">About</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                            <li><a href="single-post-music.html">Projects</a></li>
+                            <li><a href="single-post-music.html">Wordpress Themes</a></li>
+                            <li><a href="categories.html">Advertise</a></li>
+                        </ul>
+                    </aside>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <aside class="widget widget-twitter">
+                        <h4 class="widget-title">Latest Tweets</h4>
+                        <div class="tweets-container">
+                            <div id="tweets"></div>
+                        </div>
+                    </aside>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <aside class="widget widget-popular-posts">
+                        <h4 class="widget-title">Most Popular</h4>
+                        <ul class="post-list-small post-list-small--1">
+                            <li class="post-list-small__item">
+                                <article class="post-list-small__entry clearfix">
+                                    <div class="post-list-small__img-holder">
+                                        <div class="thumb-container thumb-80">
+                                            <a href="single-post-music.html">
+                                                <img data-src="img/content/post_small/post_small_46.jpg"
+                                                     src="img/empty.png" alt="" class="lazyload">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="post-list-small__body">
+                                        <h3 class="post-list-small__entry-title">
+                                            <a href="single-post-music.html">WATCH Suprise Performance, Shoots
+                                                Whiskey at Nashville’s</a>
+                                        </h3>
+                                    </div>
+                                </article>
+                            </li>
+                            <li class="post-list-small__item">
+                                <article class="post-list-small__entry clearfix">
+                                    <div class="post-list-small__img-holder">
+                                        <div class="thumb-container thumb-80">
+                                            <a href="single-post-music.html">
+                                                <img data-src="img/content/post_small/post_small_47.jpg"
+                                                     src="img/empty.png" alt="" class="lazyload">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="post-list-small__body">
+                                        <h3 class="post-list-small__entry-title">
+                                            <a href="single-post-music.html">Jack, Chris &amp; More to Headline
+                                                2018
+                                                Pilgrimage Festival</a>
+                                        </h3>
+                                    </div>
+                                </article>
+                            </li>
+                            <li class="post-list-small__item">
+                                <article class="post-list-small__entry clearfix">
+                                    <div class="post-list-small__img-holder">
+                                        <div class="thumb-container thumb-80">
+                                            <a href="single-post-music.html">
+                                                <img data-src="img/content/post_small/post_small_48.jpg"
+                                                     src="img/empty.png" alt="" class="lazyload">
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="post-list-small__body">
+                                        <h3 class="post-list-small__entry-title">
+                                            <a href="single-post-music.html">Meghan Trainor and 'Queer Eye' Cast
+                                                Team Up for Spotify ...</a>
+                                        </h3>
+                                    </div>
+                                </article>
+                            </li>
+                        </ul>
+                    </aside>
+                </div>
+
+                <div class="col-lg-3 col-md-6">
+                    <aside class="widget widget_mc4wp_form_widget">
+                        <h4 class="widget-title">Newsletter</h4>
+                        <p class="newsletter__text">
+                            <i class="ui-email newsletter__icon"></i>
+                            Subscribe for our daily news
+                        </p>
+                        <form class="mc4wp-form" method="post">
+                            <div class="mc4wp-form-fields">
+                                <div class="form-group">
+                                    <input type="email" name="EMAIL" placeholder="Your email" required="">
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-lg btn-color" value="Sign Up">
+                                </div>
+                            </div>
+                        </form>
+                    </aside>
+                </div>
+
+
+            </div> <!-- end row -->
+        </div> <!-- end footer widgets -->
+    </div> <!-- end container -->
+
+    <div class="footer__bottom footer__bottom--dark">
+        <div class="container text-center">
+            <ul class="footer__nav-menu footer__nav-menu--1">
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">News</a></li>
+                <li><a href="categories.html">Advertise</a></li>
+                <li><a href="shortcodes.html">Support</a></li>
+                <li><a href="shortcodes.html">Contact</a></li>
+            </ul>
+            <p class="copyright">
+                © 2018 Deus | Made by <a href="https://deothemes.com">DeoThemes</a>
+            </p>
+        </div>
+    </div> <!-- end footer bottom -->
+</footer> <!-- end footer -->
+
+<div id="back-to-top" style="background-color: #00BCD4;border-radius: 15%;">
+    <a href="#top" aria-label="Go to top"><i class="ui-arrow-up"></i></a>
+</div>
---
+++ View/header.php
@@ -0,0 +1,293 @@
+<!-- Preloader -->
+<div class="loader-mask">
+    <div class="loader">
+        <div></div>
+    </div>
+</div>
+
+<!-- Bg Overlay -->
+<div class="content-overlay"></div>
+
+<!-- Sidenav -->
+<header class="sidenav" id="sidenav">
+
+    <!-- close -->
+    <div class="sidenav__close">
+        <button class="sidenav__close-button" id="sidenav__close-button" aria-label="close sidenav">
+            <i class="ui-close sidenav__close-icon"></i>
+        </button>
+    </div>
+
+    <!-- Nav -->
+    <nav class="sidenav__menu-container">
+        <ul class="sidenav__menu" role="menubar">
+            <li><a href="View/" class="sidenav__menu-url">Trang chủ</a></li>
+            <li>
+                <a class="sidenav__menu-url">Bài giảng</a>
+                <button class="sidenav__menu-toggle" aria-haspopup="true" aria-label="Open dropdown"><i
+                        class="ui-arrow-down"></i></button>
+                <ul class="sidenav__menu-dropdown">
+                    <li><a href="#" class="sidenav__menu-url">Listening</a></li>
+                    <li><a href="#" class="sidenav__menu-url">Reading</a></li>
+                    <li><a href="#" class="sidenav__menu-url">Writing</a></li>
+                    <li><a href="#" class="sidenav__menu-url">Speaking</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="sidenav__menu-url">Test</a>
+                <button class="sidenav__menu-toggle" aria-haspopup="true" aria-label="Open dropdown"><i
+                        class="ui-arrow-down"></i></button>
+                <ul class="sidenav__menu-dropdown">
+                    <li><a href="shortcodes.html" class="sidenav__menu-url">Listening</a></li>
+                    <li><a href="shortcodes.html" class="sidenav__menu-url">Reading</a></li>
+                    <li><a href="shortcodes.html" class="sidenav__menu-url">Writing</a></li>
+                    <li><a href="shortcodes.html" class="sidenav__menu-url">Speaking</a></li>
+                </ul>
+            </li>
+
+            <!-- Categories -->
+            <li>
+                <a href="#" class="sidenav__menu-url">Liên hệ</a>
+            </li>
+            <li>
+                <a href="#" class="sidenav__menu-url">Giới thiệu</a>
+            </li>
+            <li>
+                <a class="sidenav__menu-url" data-toggle="modal" href="#myModal">Đăng nhập</a>
+            </li>
+            <li>
+                <a href="#" class="sidenav__menu-url">Đăng kí</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="socials sidenav__socials">
+        <a class="social social-facebook" href="#" target="_blank" aria-label="facebook">
+            <i class="ui-facebook"></i>
+        </a>
+        <a class="social social-twitter" href="#" target="_blank" aria-label="twitter">
+            <i class="ui-twitter"></i>
+        </a>
+        <a class="social social-google-plus" href="#" target="_blank" aria-label="google">
+            <i class="ui-google"></i>
+        </a>
+        <a class="social social-youtube" href="#" target="_blank" aria-label="youtube">
+            <i class="ui-youtube"></i>
+        </a>
+        <a class="social social-instagram" href="#" target="_blank" aria-label="instagram">
+            <i class="ui-instagram"></i>
+        </a>
+    </div>
+</header> <!-- end sidenav -->
+
+<!-- Top Bar -->
+<div class="top-bar d-none d-lg-block">
+    <div class="container">
+        <div class="row">
+            <!-- Socials -->
+            <div class="col-lg-12">
+                <div id="top-sign-in-out">
+                    <a class="btn btn-sm btn-light" data-toggle="modal" href="#myModal">
+                        <span>Đăng nhập</span>
+                    </a>
+                    <a href="#" class="btn btn-sm btn-light">
+                        <span>Đăng kí</span>
+                    </a>
+                </div>
+
+                <div class="socials nav__socials socials--nobase socials--white justify-content-end">
+                    <a class="social social-facebook" href="https://www.facebook.com/" target="_blank"
+                       aria-label="facebook">
+                        <i class="ui-facebook"></i>
+                    </a>
+                    <a class="social social-twitter" href="https://www.twitter.com/" target="_blank"
+                       aria-label="twitter">
+                        <i class="ui-twitter"></i>
+                    </a>
+                    <a class="social social-google-plus" href="https://www.google.com/" target="_blank"
+                       aria-label="google">
+                        <i class="ui-google"></i>
+                    </a>
+                    <a class="social social-youtube" href="https://www.youtube.com/" target="_blank"
+                       aria-label="youtube">
+                        <i class="ui-youtube"></i>
+                    </a>
+                    <a class="social social-instagram" href="https://www.instagram.com/" target="_blank"
+                       aria-label="instagram">
+                        <i class="ui-instagram"></i>
+                    </a>
+                </div>
+
+            </div>
+            <!-- The Modal -->
+            <div class="modal" id="myModal">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <form method="post" action="">
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">ĐĂNG NHẬP</h4>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                                Email:<input type="text" name="email">
+                                password:<input type="password" name="password">
+                            </div>
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button style="border:1px solid red;" type="button" class="btn"
+                                        data-dismiss="modal">Đăng nhập
+                                </button>
+                                <button type="button" class="btn" data-dismiss="modal">Hủy bỏ</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end top bar -->
+
+<!-- Navigation -->
+<header class="nav">
+
+    <div class="nav__holder nav--sticky">
+        <div class="container relative">
+            <div class="flex-parent">
+
+                <!-- Side Menu Button -->
+                <button class="nav-icon-toggle" id="nav-icon-toggle" aria-label="Open side menu">
+              <span class="nav-icon-toggle__box">
+                <span class="nav-icon-toggle__inner"></span>
+              </span>
+                </button>
+
+
+                <!-- Logo -->
+                <a href="View/" class="logo">
+                    <img class="logo__img" src="img/logo_default.png"
+                         srcset="img/logo_default.png 1x, img/logo_default@2x.png 2x" alt="logo">
+                </a>
+
+                <!-- Nav-wrap -->
+                <nav class="flex-child nav__wrap d-none d-lg-block">
+                    <ul class="nav__menu">
+
+                        <li class="active">
+                            <a href="View/">Trang chủ</a>
+                        </li>
+
+                        <li class="nav__dropdown">
+                            <a>Bài giảng</a>
+                            <ul class="nav__dropdown-menu nav__megamenu">
+                                <li>
+                                    <div class="nav__megamenu-wrap">
+                                        <div class="row">
+
+                                            <div class="col nav__megamenu-item">
+                                                <article class="entry">
+                                                    <div class="entry__img-holder">
+                                                        <a href="single-post-music.html">
+                                                            <img src="img/layout/grid_1.jpg" alt=""
+                                                                 class="entry__img">
+                                                        </a>
+                                                        <a href="categories.html"
+                                                           class="entry__meta-category entry__meta-category--label entry__meta-category--align-in-corner entry__meta-category--violet">listening</a>
+                                                    </div>
+                                                </article>
+                                            </div>
+
+                                            <div class="col nav__megamenu-item">
+                                                <article class="entry">
+                                                    <div class="entry__img-holder">
+                                                        <a href="single-post-music.html">
+                                                            <img src="img/layout/grid_2.jpg" alt=""
+                                                                 class="entry__img">
+                                                        </a>
+                                                        <a href="categories.html"
+                                                           class="entry__meta-category entry__meta-category--label entry__meta-category--align-in-corner entry__meta-category--purple">reading</a>
+                                                    </div>
+                                                </article>
+                                            </div>
+
+                                            <div class="col nav__megamenu-item">
+                                                <article class="entry">
+                                                    <div class="entry__img-holder">
+                                                        <a href="single-post-music.html">
+                                                            <img src="img/layout/grid_3.jpg" alt=""
+                                                                 class="entry__img">
+                                                        </a>
+                                                        <a href="categories.html"
+                                                           class="entry__meta-category entry__meta-category--label entry__meta-category--align-in-corner entry__meta-category--blue">writing</a>
+                                                    </div>
+                                                </article>
+                                            </div>
+
+                                            <div class="col nav__megamenu-item">
+                                                <article class="entry">
+                                                    <div class="entry__img-holder">
+                                                        <a href="single-post-music.html">
+                                                            <img src="img/layout/grid_4.jpg" alt=""
+                                                                 class="entry__img">
+                                                        </a>
+                                                        <a href="categories.html"
+                                                           class="entry__meta-category entry__meta-category--label entry__meta-category--align-in-corner entry__meta-category--green">speaking</a>
+                                                    </div>
+                                                </article>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul> <!-- end megamenu -->
+                        </li>
+
+                        <li class="nav__dropdown">
+                            <a>Test</a>
+                            <ul class="nav__dropdown-menu">
+                                <li><a href="about.html">Listening</a></li>
+                                <li><a href="contact.html">Reading</a></li>
+                                <li><a href="search-results.html">Writing</a></li>
+                                <li><a href="categories.html">Speaking</a></li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="#">Liên hệ</a>
+                        </li>
+
+                        <li>
+                            <a href="#">giới thiệu</a>
+                        </li>
+
+
+                    </ul> <!-- end menu -->
+                </nav> <!-- end nav-wrap -->
+
+                <!-- Nav Right -->
+                <div class="nav__right">
+
+                    <!-- Search -->
+                    <div class="nav__right-item nav__search">
+                        <a href="#" class="nav__search-trigger" id="nav__search-trigger">
+                            <i class="ui-search nav__search-trigger-icon"></i>
+                        </a>
+                        <div class="nav__search-box" id="nav__search-box">
+                            <form class="nav__search-form">
+                                <input type="text" placeholder="Search an article" class="nav__search-input">
+                                <button type="submit" class="search-button btn btn-lg btn-color btn-button">
+                                    <i class="ui-search nav__search-icon"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+
+                </div> <!-- end nav right -->
+
+            </div> <!-- end flex-parent -->
+        </div> <!-- end container -->
+
+    </div>
+</header>
+<!-- end navigation -->
