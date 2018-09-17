9e0fc154dcd0bb7419982576c39d17bb51cc6a8e
Thien Nguyen <ythien12345697@gmail.com>
2018-09-17 22:52:17 +0700

    Thêm giao diện admin
    Giao diện admin bootstrap 3

--- Model/config.php
+++ Model/config.php
@@ -1,8 +1,8 @@
 <?php
 	define("sql_server","localhost");
     define("sql_user","root");
-    define("sql_password","");
-	define("sql_database","toeicdb");
-	define("BASE_URL","http://localhost/ThiToeic/");
-	define("BASE_DIR","/ThiToeic/");
+    define("sql_password","thienvip123");
+	define("sql_database","quanlynguoidung");
+	define("BASE_URL","http://localhost/ToeicThi/");
+	define("BASE_DIR","/ToeicThi/");
 ?>
---
+++ admin/user_list.php
@@ -0,0 +1,73 @@
+
+<div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title text-center">Danh sách người dùng</h4>
+                                <div id="select-list-user"">
+                                <select class="form-control form-control-lg" id="user_select_box">
+                                    <option>Tất cả</option>
+                                    <option>Quản trị viên</option>
+                                    <option>Giảng viên</option>
+                                    <option>Học viên</option>
+                                </select></div></br>
+                                <p class="category"><button type="button" class="btn btn-info"><i class="fa fa-plus"></i>Thêm người dùng</button></p>
+                               
+                            
+                            <div class="content table-responsive table-full-width">
+                                <table class="table table-hover table-striped">
+                                    <thead>
+                                        <th>ID</th> 
+                                    	<th>Name</th>
+                                    	<th>Salary</th>
+                                    	<th>Country</th>
+                                    	<th>City</th>
+                                        <th>Action</th>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            include("connect.php");
+                                            $sql = "SELECT * FROM user";
+                                            $result = mysql_query($sql);
+                                            if(mysql_num_rows($result) > 0)
+                                            {
+                                                $i = 1;
+                                                while($row = mysql_fetch_assoc($result))
+                                                {
+                                                    echo '<tr><td>'.$i.'</td><td>'.$row["Username"].'</td><td>'.$row["Username"].'</td><td>'.$row["Username"].'</td><td>'.$row["Username"].'</td><td class="td-actions">
+                                                    <a href="index.php?u=edit?uid='.$row["Username"].'"><button type="button" rel="tooltip" title="Edit Task" class="btn btn-info btn-simple btn-xs">
+                                                        <i class="fa fa-edit"></i>
+                                                    </button></a>
+                                                    <a href="index.php?u=remove?uid='.$row["Username"].'"><button type="button" rel="tooltip" title="Remove" class="btn btn-danger btn-simple btn-xs">
+                                                        <i class="fa fa-times"></i>
+                                                    </button></a>
+                                                </td></tr>';
+
+                                                    $i++;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                echo '<tr><td>No record</td><td>No record</td><td>No record</td><td>No record</td><td>No record</td></tr>';
+                                            }
+                                            mysql_close();
+                                        ?>
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                            
+                            <form class="form-inline active-pink-4">
+                                <input class="form-control form-control-sm mr-3 w-75" type="text" aria-label="Search">
+                                <i class="fa fa-search" aria-hidden="true"></i>
+                                <span>Tìm kiếm</span>
+                        </form><br>
+                   
+                    
+                </div>
+            </div>
+        </div>
